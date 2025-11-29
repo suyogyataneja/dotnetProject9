8d7aa9608d474bda6ee542992ed551dabf92a20b
Suyogya Taneja <Suyogyataneja@gmail.com>
2025-11-29 23:24:31 +1100

    Create Activity form component. Refactor the code for NavBar.tsx, App.tsx, ActivityDashboard.tsx, ActivityForm.tsx and ActivityDetail.tsx

---
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -0,0 +1,38 @@
+import { Box, Button, Paper, TextField, Typography } from '@mui/material'
+import React from 'react'
+
+type Props ={
+activity?:Activity;
+closeForm:()=>void;
+    
+}
+
+export default function ActivityForm({activity,closeForm}:Props) {
+  return (
+    <Paper sx={{borderRadius:3, padding:3}}>
+
+        <Typography variant="h5" gutterBottom color="primary">
+            CreateActivity
+        </Typography>
+
+        <Box component ='form' display='flex' flexDirection='column' gap={3}>
+            <TextField label='Title' value={activity?.title} />
+            <TextField label='Description' multiline rows={3} />
+            <TextField label='Category' />
+            <TextField label='Date' type='date' />
+            <TextField label='City' />
+            <TextField label='Venue' />
+
+            <Box display='flex' justifyContent='end'gap={3} >
+                <Button onClick={closeForm}  color='inherit'>Cancel</Button>
+                <Button color='success' variant="contained">Submit</Button>
+
+            </Box>
+
+        </Box>
+
+
+
+    </Paper>
+  )
+}
