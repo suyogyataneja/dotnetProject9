6193bcd44ceb2270c45e93cd8184fe6b0f27b4cb
Suyogya Taneja <Suyogyataneja@gmail.com>
2025-11-30 23:43:19 +1100

    Update the default values for ActivityForm.

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -16,12 +16,12 @@ export default function ActivityForm({activity,closeForm}:Props) {
         </Typography>
 
         <Box component ='form' display='flex' flexDirection='column' gap={3}>
-            <TextField label='Title' value={activity?.title} />
-            <TextField label='Description' multiline rows={3} />
-            <TextField label='Category' />
-            <TextField label='Date' type='date' />
-            <TextField label='City' />
-            <TextField label='Venue' />
+            <TextField label='Title' defaultValue={activity?.title} />
+            <TextField label='Description' defaultValue={activity?.description} multiline rows={3} />
+            <TextField label='Category' defaultValue={activity?.category} />
+            <TextField label='Date' type='date' defaultValue={activity?.date} />
+            <TextField label='City' defaultValue={activity?.city}/>
+            <TextField label='Venue' defaultValue={activity?.venue}/>
 
             <Box display='flex' justifyContent='end'gap={3} >
                 <Button onClick={closeForm}  color='inherit'>Cancel</Button>
