b15f21e89ee63038118af48fddb8dbbf5afb803a
Suyogya Taneja <Suyogyataneja@gmail.com>
2025-12-10 23:26:25 +1100

    Create a Routes file. Replace App with RouteProvider in the main.tsx file.

---
+++ client/src/app/router/Routes.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router";
+import App from "../layout/App";
+
+export const router = createBrowserRouter([
+
+    {
+
+        path :'/',
+        element:<App />
+
+    }
+])
