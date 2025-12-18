4b76d8d642edc91fdf1998dd000d3e2ab06cf37f
Suyogya Taneja <Suyogyataneja@gmail.com>
2025-12-18 23:54:48 +1100

    Refactor the code

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -44,3 +44,39 @@ export default function NavBar() {
     </Box>
   )
 }
+
+
+export default function NavBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx ={{backgroundImage:'linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac )'}}>
+
+        <Container maxWidth= 'xl'>
+            <Toolbar sx={{display:'flex', justifyContent:'space-between'}}>
+                <Box>
+                    <MenuItem component={NavLink} to='/' sx= {{display:'flex', gap:2}} >
+                    <Group fontSize ="large"/>
+                    <Typography variant="h4" fontWeight='bold'> Reactivities</Typography>
+                    </MenuItem>
+                </Box>
+
+                <Box sx={{display:'flex'}}>
+                    <MenuItemLink  to='/activities' >
+                    Activities
+                    </MenuItemLink>
+
+                    <MenuItemLink
+                    to='/createActivity'>
+                    Create Activity
+                    </MenuItemLink>
+
+                </Box>
+
+
+            </Toolbar>
+        </Container>
+      
+      </AppBar>
+    </Box>
+  )
+}
