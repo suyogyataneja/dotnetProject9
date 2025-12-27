2e3799eb6757a89c38d6bedc833c05c062718bf5
Suyogya Taneja <Suyogyataneja@gmail.com>
2025-12-27 22:26:17 +1100

    Comment the code

--- Persistence/Repositories/ActivityRepository.cs
+++ Persistence/Repositories/ActivityRepository.cs
@@ -1,8 +1,33 @@
+// Persistence/Repositories/ActivityRepository.cs
 using Application.Interfaces;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
 namespace Persistence.Repositories;
 
-
-public class ActivityRepository: IActivityRepository
+public class ActivityRepository : IActivityRepository
 {
-    
+    private readonly AppDbContext _context;
+
+    public ActivityRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    // public async Task<List<Activity>> GetAllAsync() =>
+    //     await _context.Activities.ToListAsync();
+    //
+    // public async Task<Activity?> GetByIdAsync(Guid id) =>
+    //     await _context.Activities.FindAsync(id);
+    //
+    // public async Task AddAsync(Activity activity) =>
+    //     await _context.Activities.AddAsync(activity);
+    //
+    // public void Update(Activity activity) =>
+    //     _context.Activities.Update(activity);
+    //
+    // public void Delete(Activity activity) =>
+    //     _context.Activities.Remove(activity);
+    //
+    // public async Task<bool> SaveChangesAsync() =>
+    //     await _context.SaveChangesAsync() > 0;
 }
