bf6ff9fbfd5db78d20278c8d8359b8f5c476c4f8
Suyogya Taneja <Suyogyataneja@gmail.com>
2026-01-01 23:08:19 +1100

    Refactor the code

--- Application/Activities/Queries/GetActivityDetails.cs
+++ Application/Activities/Queries/GetActivityDetails.cs
@@ -11,23 +11,25 @@ public class GetActivityDetails
 
     public class Query : IRequest<Activity>
     {
-        public Guid Id { get; set; }
+        // public Guid Id { get; set; }
+        
+        public string Id { get; set; } = string.Empty;
         // public string Id { get; set; } = Guid.NewGuid().ToString();
         
     }
 
     // Public class Handler
-    // public class Handler(AppDbContext context) : IRequestHandler<Query, Activity>
-    public class Handler(IActivityRepository repository) :IRequestHandler<Query, Activity>
+     public class Handler(AppDbContext context) : IRequestHandler<Query, Activity>
+    //public class Handler(IActivityRepository repository) :IRequestHandler<Query, Activity>
     {
-
 
         public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
         {
 
-            // var activity = await context.Activities.FindAsync(request.Id, cancellationToken);
+             var activity = await context.Activities.FindAsync(request.Id, cancellationToken);
             // var activity = await repository.GetByIdAsync(request.Id);
-            var activity = await repository.GetByIdAsync(request.Id);
+            // var activity = await repository.GetByIdAsync(request.Id);       
+            
             // WE Dont have ability to return HTTP RESPONSE CODES here
             if (activity == null) throw new Exception("Activity not found");
             return activity;
