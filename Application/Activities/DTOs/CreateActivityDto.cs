0c0bb162671c5ba3d04c402ec85e89bb2e3a7911
Suyogya Taneja <Suyogyataneja@gmail.com>
2026-01-14 23:14:23 +1100

    Remove the [Required] keywords for validation and instead validate using Fluent Validation.

--- Application/Activities/DTOs/CreateActivityDto.cs
+++ Application/Activities/DTOs/CreateActivityDto.cs
@@ -4,22 +4,19 @@ namespace Application.Activities.DTOs;
 
 public class CreateActivityDto
 {
-    [Required]
-    public string Title { get; set; }  // anything required cant be null
-    [Required]
+    public string Title { get; set; } = ""; // anything required cant be null
+    
     public DateTime Date { get; set; }
-    [Required]
+    
     public string Description { get; set; }
-    [Required]
-    public string Category { get; set; }
-    //location props
-    [Required]
-    public string City { get; set; }
-    [Required]
-    public string Venue { get; set; }
-    [Required]
-    public double  Latitude { get; set; }
-    [Required]
-    public double  Longitude { get; set; }
 
+    public string Category { get; set; } = "";
+    //location props
+    public string City { get; set; } = "";
+
+    public string Venue { get; set; } = "";
+    
+    public double  Latitude { get; set; }
+
+    public double  Longitude { get; set; }
 }
