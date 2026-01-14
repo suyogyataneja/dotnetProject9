2eb57a7ec01e5e23d1271fd4367070ac242644cf
Suyogya Taneja <Suyogyataneja@gmail.com>
2026-01-14 23:15:29 +1100

    Refactor the CreateActivity handler to validate using CreateActivityValidator.

--- Application/Activities/Commands/CreateActivity.cs
+++ Application/Activities/Commands/CreateActivity.cs
@@ -1,6 +1,7 @@
 using Application.Activities.DTOs;
 using AutoMapper;
 using Domain;
+using FluentValidation;
 using MediatR;
 using Persistence;
 
@@ -14,11 +15,12 @@ public class CreateActivity
         public required CreateActivityDto ActivityDto { get; set; }
     }
     
-    public class Handler(AppDbContext context, IMapper mapper) : IRequestHandler<Command, string>
+    public class Handler(AppDbContext context, IMapper mapper, IValidator<Command> validator) : IRequestHandler<Command, string>
     {
         public async Task<string> Handle(Command request, CancellationToken cancellationToken)
         {
-            
+
+            await validator.ValidateAndThrowAsync(request, cancellationToken);
             var activity = mapper.Map<Activity>(request.ActivityDto);
             // context.Activities.Add(request.Activity);            
             context.Activities.Add(activity);           
