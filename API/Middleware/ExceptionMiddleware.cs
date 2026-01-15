5ec3f6caa002a2de46f3c6cd445a99e975041871
Suyogya Taneja <Suyogyataneja@gmail.com>
2026-01-15 23:18:17 +1100

    Create an Exception Middleware.

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,58 @@
+
+using FluentValidation;
+
+namespace API.Middleware;
+
+public class ExceptionMiddleware:IMiddleware 
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next) 
+    {
+
+        try
+        {
+            await next(context); 
+        }
+
+        catch (ValidationException ex)
+        {
+            await HandleValidationException(context, ex);
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+
+    private static async Task HandleValidationException(HttpContext context, ValidationException ex)
+    {
+        
+        var validationErrors = new Dictionary<string, string[]>();
+
+        if (ex.Errors is not null)
+        {
+            foreach (var error in ex.Errors)
+            {
+                if (validationErrors.TryGetValue(error.PropertyName, out var existingErrors))
+                {
+                    validationErrors[error.PropertyName] = existingErrors.Append(error.ErrorMessage).ToArray();
+                }
+                else
+                {
+                    validationErrors[error.PropertyName] = [error.ErrorMessage];
+                }
+            }
+        }
+        
+        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+
+        var validationProblemDetails = new HttpValidationProblemDetails(validationErrors)
+        {
+            Status = StatusCodes.Status400BadRequest,
+            Type = "ValidationFailure",
+            Title = "One or more validation errors occurred."
+        };
+        // throw new NotImplementedException();
+        await context.Response.WriteAsJsonAsync(validationProblemDetails);
+    }
+}
