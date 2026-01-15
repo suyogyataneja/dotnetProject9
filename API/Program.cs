502cb7c18d95ec062132b5b6fc992461dfef1d59
Suyogya Taneja <Suyogyataneja@gmail.com>
2026-01-15 23:18:00 +1100

    Inject ValidationBehaviour class in MediatR in program class. Add Exception Middleware as an Add Transient service in program class.

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using API.Middleware;
 using Application.Activities.Queries;
 using Application.Activities.Validators;
 using Application.Core;
@@ -24,12 +25,21 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 // Adding CORS 
 builder.Services.AddCors();
 
-builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
+// builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
+
+builder.Services.AddMediatR(x =>
+{
+    x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>();
+    x.AddOpenBehavior(typeof(ValidationBehavior<,>));
+});
+
 //  Register custom services (e.g., repositories, application services)
 // builder.Services.AddScoped<IYourService, YourService>();
 
 builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 builder.Services.AddValidatorsFromAssemblyContaining<CreateActivityValidator>();
+builder.Services.AddTransient<ExceptionMiddleware>(); // this exceptiomiddleware is only created when there's an exception
+
 
 builder.Services.AddScoped<IActivityRepository, ActivityRepository>();
 
@@ -38,6 +48,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 // Configure the HTTP request pipeline.
 //ADDING CORS CONFIGURATION
