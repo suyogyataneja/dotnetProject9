7a0dd7b277fb214c14788b6d57506ba50803bafa
Suyogya Taneja <Suyogyataneja@gmail.com>
2026-01-16 23:30:39 +1100

    Refactor the code

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -60,7 +60,9 @@ public class ActivitiesController() : BaseApiController
    [HttpGet("{id}")]
    public async Task<ActionResult<Activity>> GetActivityById(string id)
    {
-      return await Mediator.Send(new GetActivityDetails.Query { Id = id });
+      
+      return HandleResult(await Mediator.Send(new GetActivityDetails.Query { Id = id }));
+
    }
 
    [HttpPost]
