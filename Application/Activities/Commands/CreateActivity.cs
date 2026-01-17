a13e49a85ee32992ddba5cc8bd600679ca0ec629
Suyogya Taneja <Suyogyataneja@gmail.com>
2026-01-18 00:04:17 +1100

    Update the CreateActivity.cs with the Result return type.

--- Application/Activities/Commands/CreateActivity.cs
+++ Application/Activities/Commands/CreateActivity.cs
@@ -1,4 +1,5 @@
 using Application.Activities.DTOs;
+using Application.Core;
 using AutoMapper;
 using Domain;
 using FluentValidation;
@@ -9,15 +10,15 @@ namespace Application.Activities.Commands;
 
 public class CreateActivity
 {
-    public class Command : IRequest<string>
+    public class Command : IRequest<Result<string>>
     {
         
         public required CreateActivityDto ActivityDto { get; set; }
     }
     
-    public class Handler(AppDbContext context, IMapper mapper) : IRequestHandler<Command, string>
+    public class Handler(AppDbContext context, IMapper mapper) : IRequestHandler<Command, Result<string>>
     {
-        public async Task<string> Handle(Command request, CancellationToken cancellationToken)
+        public async Task<Result<string>> Handle(Command request, CancellationToken cancellationToken)
         {
             
             // await validator.ValidateAndThrowAsync(request, cancellationToken);
@@ -25,11 +26,12 @@ public class CreateActivity
             // context.Activities.Add(request.Activity);            
             context.Activities.Add(activity);           
             
-            await context.SaveChangesAsync(cancellationToken);
+            var result = await context.SaveChangesAsync(cancellationToken)>0;
             
+            if(!result) return Result<string>.Failure("Failed to create activity ",400);
             // return request.Activity.Id;
 
-            return activity.Id;
+            return Result<string>.Success(activity.Id);
         }
     }
 }
