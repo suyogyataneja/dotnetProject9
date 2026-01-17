9da439ba70b748910e684c20c30980075987620a
Suyogya Taneja <Suyogyataneja@gmail.com>
2026-01-17 23:50:28 +1100

    Refactor the code

--- Application/Activities/Commands/EditActivity.cs
+++ Application/Activities/Commands/EditActivity.cs
@@ -1,3 +1,4 @@
+using Application.Core;
 using AutoMapper;
 using Domain;
 using MediatR;
@@ -9,25 +10,30 @@ public class EditActivity
 {
     
     
-    public class Command: IRequest
+    public class Command: IRequest<Result<Unit>>
     {
         public required Activity Activity { get; set; }
     }
     
-    public class Handler(AppDbContext context,IMapper mapper):IRequestHandler<Command>
+    public class Handler(AppDbContext context,IMapper mapper):IRequestHandler<Command,Result<Unit>>
     {
-        public async Task Handle(Command request, CancellationToken cancellationToken)
+        public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
         {
             var activity = await context.Activities.FindAsync([request.Activity.Id], cancellationToken);
             
-            if (activity == null) throw new Exception("Activity not found/Cannot find activity with id:{id}");
-
+            // if (activity == null) throw new Exception("Activity not found/Cannot find activity with id:{id}");
+            
+            if(activity == null) return Result<Unit>.Failure("Activity not found",404);
+            
             // activity.Title = request.Activity.Title;
             // lets use AutoMapper here
             mapper.Map(request.Activity,activity);
             
-            await context.SaveChangesAsync(cancellationToken);
-
+            var result = await context.SaveChangesAsync(cancellationToken) > 0;
+            
+            if(!result) return Result<Unit>.Failure("Failed to update activity",400);
+            return Result<Unit>.Success(Unit.Value);
+                
             // return Unit.Value;
 
         }
