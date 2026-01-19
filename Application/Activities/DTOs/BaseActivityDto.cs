ee5e710a3fb1f511f3cd3f2525588655a0966c52
Suyogya Taneja <Suyogyataneja@gmail.com>
2026-01-19 13:36:34 +1100

    Add BaseActivityDto to centralize common activity properties.

---
+++ Application/Activities/DTOs/BaseActivityDto.cs
@@ -0,0 +1,20 @@
+namespace Application.Activities.DTOs;
+
+public class BaseActivityDto
+{
+    public string? Title { get; set; } = ""; // anything required cant be null
+    
+    public DateTime Date { get; set; }
+    
+    public string? Description { get; set; }
+
+    public string? Category { get; set; } = "";
+    //location props
+    public string? City { get; set; } = "";
+
+    public string? Venue { get; set; } = "";
+    
+    public double  Latitude { get; set; }
+
+    public double  Longitude { get; set; }   
+}
