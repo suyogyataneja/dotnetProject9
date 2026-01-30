e32f3d547ad26716bf78cebb3bc7750230bda121
Suyogya Taneja <Suyogyataneja@gmail.com>
2026-01-31 00:11:58 +1100

    Add EF Core migration after updating User and Identity configuration

---
+++ Persistence/Migrations/20260130130732_SyncIdentityModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncIdentityModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DisplayImage",
+                table: "AspNetUsers",
+                newName: "DisplayName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DisplayName",
+                table: "AspNetUsers",
+                newName: "DisplayImage");
+        }
+    }
+}
