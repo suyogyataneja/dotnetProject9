80e89753e43d6455c0ba6bc7c91e7b6dde7875b8
Suyogya Taneja <Suyogyataneja@gmail.com>
2026-02-18 22:04:40 +1100

    (activity-form): add form submission handler(handleSubmit) and extract form values using FormData function.

--- client/src/features/form/ActivityForm.tsx
+++ client/src/features/form/ActivityForm.tsx
@@ -1,4 +1,5 @@
 import { Box, Button, Paper, TextField, Typography } from "@mui/material";
+import type { FormEvent } from "react";
 
 type Props = {
      // property name 'closeForm' of type function that takes no arguments and returns void: type of data we expect to receive from parent component
@@ -7,20 +8,39 @@ type Props = {
 }
 
 export default function ActivityForm({ activity, closeForm }: Props) {
+
+const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+
+    // Handle form submission logic here
+    // You can access the form values using the state or refs
+    // For example, you can create a new activity object and pass it to a parent component or make an API call to save it
+    // const newActivity = {}
+    event.preventDefault(); // Prevent the default form submission behavior
+    // console.log(event);
+    const formData = new FormData(event.currentTarget);
+
+    const data:{[key: string]: FormDataEntryValue} = {}
+    formData.forEach((value, key) => {
+        data[key] = value;
+    });
+    console.log(data);
+}
+
+
   return (
     <Paper sx={{borderRadius: 3, padding: 2}}>
         <Typography variant="h5" gutterBottom color="primary" >  
         Create Activity
         </Typography>
 
-        <Box component="form" display="flex" flexDirection="column" gap={3}>
-            <TextField label="Title" defaultValue={activity?.title}/>
+        <Box component="form" onSubmit={handleSubmit} display="flex" flexDirection="column" gap={3}>
+            <TextField name='title' label="Title" defaultValue={activity?.title}/>
 
-            <TextField label="Description" defaultValue={activity?.description} multiline rows={3} />
-            <TextField label="Category" defaultValue={activity?.category} />
-            <TextField label="Date" type="date" defaultValue={activity?.date} />
-            <TextField label="City" defaultValue={activity?.city} />
-            <TextField label="Venue" defaultValue={activity?.venue} />
+            <TextField name='description' label="Description" defaultValue={activity?.description} multiline rows={3} />
+            <TextField name='category' label="Category" defaultValue={activity?.category} />
+            <TextField name='date' label="Date" type="date" defaultValue={activity?.date} />
+            <TextField name='city' label="City" defaultValue={activity?.city} />
+            <TextField name='venue' label="Venue" defaultValue={activity?.venue} />
 
             <Box display='flex' justifyContent='space-between' alignItems='center' gap={2}>
 
@@ -28,7 +48,7 @@ export default function ActivityForm({ activity, closeForm }: Props) {
                     Cancel
                 </Button>
 
-                <Button  color="success"  variant="contained" fullWidth>
+                <Button  type="submit" color="success"  variant="contained" fullWidth>
                     Submit
                 </Button>
 
