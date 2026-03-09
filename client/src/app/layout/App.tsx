6be74ea1d7477f98c644efde39b4b0cae8427162
Suyogya Taneja <Suyogyataneja@gmail.com>
2026-03-09 17:08:12 +1100

    Remove handle submit form

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -49,22 +49,6 @@ function App() {
     setEditMode(false);
   }
 
-  const handleSubmitForm = (activity: Activity) => {
-
-    // if(activity.id) {
-
-    //   setActivities(activities.map(x=> x.id === activity.id ? activity : x));
-
-    // }
-    //   else {
-    //     const newActivity = {...activity, id: activities.length.toString()}; 
-    //     setSelectedActivity(newActivity); 
-    //     setActivities([...activities, newActivity]);
-
-    //   }
-    console.log(activity);
-    setEditMode(false);
-  }
 
 
   const handleDelete = (id: string) => {
