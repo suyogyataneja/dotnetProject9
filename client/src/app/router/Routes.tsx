be7e9597244394e6f38fe5f2d35c64140f6e336a
Suyogya Taneja <Suyogyataneja@gmail.com>
2026-03-24 22:56:59 +1100

    Update the comments

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -3,7 +3,7 @@ import App from "../layout/App";
 
 export const router = createBrowserRouter([
   {
-    path  : "/",
-    element : <App/>
+    path  : "/", // This is the root path of the application
+    element : <App/> // This specifies that when the user navigates to the root path, the App component will be rendered
   }
 ])
